a5ea9230ce75a5ef71f638da9e2af1620e28bf4d
sroshchepkin <sroshchepkin@gotrg.com>
2023-03-09 21:26:45 +0100

    add serilog as logger

--- RestaurantManager.API/Program.cs
+++ RestaurantManager.API/Program.cs
@@ -7,83 +7,107 @@ using RestaurantManager.API.Interfaces;
 using RestaurantManager.API.Models;
 using RestaurantManager.API.Persistence;
 using RestaurantManager.API.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                   .Build();
 
-builder.Services.AddControllers(options => { options.Filters.Add<GlobalExceptionFilter>(); });
+Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
 
-builder.Services.AddApiVersioning(opt =>
+try
 {
-    opt.DefaultApiVersion = new ApiVersion(1, 0);
-    opt.AssumeDefaultVersionWhenUnspecified = true;
-    opt.ReportApiVersions = true;
-    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
-        new HeaderApiVersionReader("x-api-version"),
-        new MediaTypeApiVersionReader("x-api-version"));
-});
+    Log.Information("Starting web host");
 
-// Add ApiExplorer to discover versions
-builder.Services.AddVersionedApiExplorer(setup =>
-{
-    setup.GroupNameFormat = "'v'VVV";
-    setup.SubstituteApiVersionInUrl = true;
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IClientsGroupsRepository, ClientsGroupsInMemoryRepository>();
+    builder.Host.UseSerilog();
 
-var tables = builder.Configuration
-                    .GetSection(TablesOptions.Tables)
-                    .Get<int[]>().Select(size => new Table(size))
-                    .ToList();
+    // Add services to the container.
+    builder.Services.AddControllers(options => { options.Filters.Add<GlobalExceptionFilter>(); });
 
-builder.Services.AddSingleton<ITablesRepository, TablesInMemoryRepository>(
-    serviceProvider => new TablesInMemoryRepository(
-        tables: tables
-    ));
-
-builder.Services.AddHostedService<QueuedHostedService>();
-
-builder.Services.AddSingleton<IRestaurantManager, RestaurantManagerService>();
-
-builder.Services.AddSingleton<IBackgroundTaskQueue>(context =>
-{
-    if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
-        queueCapacity = 10;
-    return new BackgroundTaskQueue(queueCapacity);
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
-
-builder.Services.AddHealthChecks();
-
-var app = builder.Build();
-
-app.MapHealthChecks("/hc");
-
-app.UseHttpLogging();
-
-var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    builder.Services.AddApiVersioning(opt =>
     {
-        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
-        {
-            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
-                description.GroupName.ToUpperInvariant());
-        }
+        opt.DefaultApiVersion = new ApiVersion(1, 0);
+        opt.AssumeDefaultVersionWhenUnspecified = true;
+        opt.ReportApiVersions = true;
+        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+            new HeaderApiVersionReader("x-api-version"),
+            new MediaTypeApiVersionReader("x-api-version"));
     });
+
+    // Add ApiExplorer to discover versions
+    builder.Services.AddVersionedApiExplorer(setup =>
+    {
+        setup.GroupNameFormat = "'v'VVV";
+        setup.SubstituteApiVersionInUrl = true;
+    });
+
+    builder.Services.AddSingleton<IClientsGroupsRepository, ClientsGroupsInMemoryRepository>();
+
+    var tables = builder.Configuration
+                        .GetSection(TablesOptions.Tables)
+                        .Get<int[]>().Select(size => new Table(size))
+                        .ToList();
+
+    builder.Services.AddSingleton<ITablesRepository, TablesInMemoryRepository>(
+        serviceProvider => new TablesInMemoryRepository(
+            tables: tables
+        ));
+
+    builder.Services.AddHostedService<QueuedHostedService>();
+
+    builder.Services.AddSingleton<IRestaurantManager, RestaurantManagerService>();
+
+    builder.Services.AddSingleton<IBackgroundTaskQueue>(context =>
+    {
+        if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
+            queueCapacity = 10;
+        return new BackgroundTaskQueue(queueCapacity);
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+
+    app.MapHealthChecks("/hc");
+
+    var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant());
+            }
+        });
+    }
+
+    app.UseSerilogRequestLogging();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
